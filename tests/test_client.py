05e81035cbd7bfaa29e890fd177931e5ce8347bd
Bradley Marques <bradleyrcmarques@gmail.com>
2024-07-17 16:52:15 +0200

    Client can be initialized with an existing token

--- tests/test_client.py
+++ tests/test_client.py
@@ -19,3 +19,10 @@ class TestClient:
         assert client.access_token is None
         assert client.login() is True
         assert client.access_token == "access-token-123"
+
+    def test_can_be_initialized_with_an_existing_token(self, mock_auth_callback_server):
+        existing_token: str = "my-existing-token-123"
+        client: Client = Client(access_token=existing_token)
+        assert client.is_logged_in()
+
+        mock_auth_callback_server.assert_not_called()
